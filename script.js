2efe1ffef2c46860cd8a5808db4ece2f13f3b19e
blue Developer <115267637+Ahmdadm@users.noreply.github.com>
2024-05-14 15:41:00 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+
+let code = [
+  {
+    title:"Hi!"
+  },
+  {
+    title:"دكتور"
+  },
+  {
+    title:"مهندس!"
+  },
+  {
+    title:"عامل توصيل مياه"
+  },
+  {
+    title:"خدمات صحية"
+  },
+  {
+    title:"خدمات مالية"
+  },
+  {
+    title:"بيت للعجار"
+  },
+  {
+    title:"سياره للعجار"
+  },
+  {
+    title:"عامل نظافه"
+  },
+  {
+    title:"خدمات توصيل"
+  },
+  {
+    title:"نجار"
+  },
+  {
+    title:"عامل بناء"
+  },
+  {
+    title:"مدرس"
+  },
+  {
+    title:"اخر"
+  },
+
+
+];
+
+const section = document.querySelector("#section");
+
+let html = '';
+code.forEach((items, indext) =>{
+ html += ` 
+<div class="column">
+<a href="#">
+<div class="card">
+    <!-- <img src="imges/dog.jpg" alt="" class="thumbnall"> -->
+    <h3>${code[indext].title}</h3>
+  </div>
+
+</a>
+  
+</div>
+
+`
+})
+
+section.innerHTML = html;
+console.log(code)
